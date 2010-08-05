dd04fc198ecbac07446337d3e0f0b7320fe6f7be
Kwpolska <kwpolska@gmail.com>
2010-08-05 17:37:09 +0200

    need to show the file

--- kwdocumentator/kwd.pl
+++ kwdocumentator/kwd.pl
@@ -3,29 +3,58 @@
 # Part of KRU
 # Copyright Kwpolska 2010. Licensed on GPLv2.
 use warnings;
+use Term::ANSIColor;
 print "KwDocumentator\nCopyright Kwpolska 2010. Licensed on GPLv2.\n\n";
-
-
-print "1. Main program information\ne: Kw's Pastebin. Minimalistic as hell.\n> ";
+sub printblue {
+my $value=shift;
+print color 'blue';
+print '['.$value.'] ';
+print color 'reset';
+}
+printblue("1");
+print "Main program information\n";
 my $maininfo = <STDIN>;
 
-print "2. Purpose\ne: The pastebin was written originally on request for personal use. Now, the project was published.\n> ";
+print "Purpose\n";
 my $purpose = <STDIN>;
 
-print "3. Install instructions. \ne: Check out the INSTALL file. I promise it wouldn't take long. The project requires PHP 5.1 or newer. PHP 5.0 requires getting PDO.\n> ";
+print "Install instructions\n";
 my $instructions = <STDIN>;
 
-print "4. Save the file as:\n> ";
-my $location = <STDIN>;
-chomp($location);
+print "Additional notes - leave empty if needed\n";
+my $notesbase = <STDIN>;
 
+print "Save the file as:\n";
+my $location = <STDIN>;
+
+print "Preparing... ";
+chomp($maininfo);
+chomp($purpose);
+chomp($instructions);
+chomp($location);
+chomp($notesbase);
+my $notes = '';
+if ($notesbase ne "") {
+$notes = "\nNOTES:\n--------------\n$notesbase\n";
+}
+print "done\n";
 print "Saving data... ";
 open($fh, '>', $location) or die "Wrong chmods to $!";
 #you can edit the template here:
 print $fh <<THEND;
 $maininfo
+--------------
+
+PURPOSE:
+--------------
 $purpose
+
+INSTALLATION:
+--------------
 $instructions
+$notes
+COPYRIGHT:
+--------------
 Copyright (C) 2010 Kwpolska.
 
 This program is free software; you can redistribute it and/or
@@ -43,5 +72,5 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 THEND
 close $fh;
-print "done\a\n";
+print "done\n";
 
