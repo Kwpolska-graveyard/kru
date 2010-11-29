f45b87bbccd8bbd6a21dcd9d8cbbfa8f9effa3cf
Kwpolska <kwpolska@gmail.com>
2010-11-30 00:00:07 +0100

    auto commit

--- pkgbuilder/build.pl
+++ pkgbuilder/build.pl
@@ -8,26 +8,40 @@ use warnings;
 use strict;
 use LWP::Simple;
 use Archive::Any;
-
+use Term::ANSIColor;
+sub info {
+	print color 'bold green';
+	print "==> ";
+	print color 'reset';
+	print color 'bold';
+	print shift."\n";
+	print color 'reset';
+}
 sub generate {
 	my $pkg=shift;
+	info("Building package ".$pkg."...");
 	my $tar=$pkg.".tar.gz";
 	my $url="http://aur.archlinux.org/packages/".$pkg."/".$tar;
 
 #let's download the file.
+	info("Downloading the tar file...");
 	getstore($url, "./".$tar);
 
 #untar it...
+	info("Extracting the tar file...");
 	my $archive = Archive::Any->new("./".$tar);
 	$archive->extract;
+
+#build it.
 	system('cd '.$pkg.'; time makepkg -si');
+	info("The build finished.\n");
 }
 
 my $argc = $#ARGV + 1;
 my $i = 0;
 if ($argc eq '0') {
-print "ERROR: No package specified.
-HINT:  ./build.pl pkg [pkg2]\n";
+	print "ERROR: No package specified.
+		HINT:  ./build.pl pkg [pkg2]\n";
 }
 while ($i != $argc) {
 	my $arg = $ARGV[$i]; #There was no other way.
