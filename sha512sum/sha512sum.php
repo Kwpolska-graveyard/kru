90a7a2ed88800452bbaa3fee37e640a5bd1c02ac
Kwpolska <kwpolska@gmail.com>
2011-02-04 16:05:18 +0100

    auto commit

--- sha512sum/sha512sum.php
+++ sha512sum/sha512sum.php
@@ -1,7 +1,7 @@
 <?php
 //sha512sum
 //By Kwpolska. On PD.
-$saltbase = 'jekyllforrubyftw'; //up to 16 characters, longer ones will be trimmed
+$saltbase = 'somerandomsalt16'; //up to 16 characters, longer ones will be trimmed
 $salt = '$6$rounds=5000$'.$saltbase.'$';
 if($_SERVER['REQUEST_METHOD'] == 'POST') echo crypt($_POST['passwd'], $salt).'<br>';
 echo '<form method="post" action="?"><input type="password" name="passwd"> <input type="submit" value="Generate"></form>';
