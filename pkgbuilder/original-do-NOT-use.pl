4e858b652b6f67a1cb14c23184d0fce7cc26e4a1
Kwpolska <kwpolska@gmail.com>
2011-07-31 11:13:54 +0200

    pkgbuilder: HUGE change. The new build.py is 200% better and 20% cooler.

--- pkgbuilder/original-do-NOT-use.pl
+++ pkgbuilder/original-do-NOT-use.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
-# PKGBUILDer/build.pl
+# PKGBUILDer
 # Part of KRU
-# Copyright Kwpolska 2010-2011. Licensed under GPLv3.
-#USAGE: ./build packages
+# Copyright Kwpolska 2010. Licensed on GPLv3.
+#USAGE: ./build pkg1 [pkg2] [pkg3] (and more)
 
 use warnings;
 use strict;
@@ -15,64 +15,42 @@ $SIG{'HUP' } = 'freset';  $SIG{'TRAP'} = 'freset';
 $SIG{'ABRT'} = 'freset';  $SIG{'STOP'} = 'freset';
 
 sub info {
-    print BOLD, GREEN, "==> ", RESET, BOLD, shift."\n", RESET;
+   print BOLD, GREEN, "==> ", RESET, BOLD, shift."\n", RESET;
 }
 
 sub freset {
-    print RESET, BOLD, "Okay!\nExiting right now.\n", RESET;
-    exit(1);
+   print RESET, BOLD, "Okay!\nExiting right now.\n", RESET;
+   exit(1);
 }
 
-sub help {
-    print BOLD, "PKGBUILDer/build.pl", RESET;
-    print "Copyright Kwpolska 2010-2011. Licensed under GPLv3.";
-    print "Usage: ./build.pl [PACKAGE]...";
-#    print "Usage: ./build.pl [-dur] [--download] [--get-unpack] [--root] [PACKAGE]...
-#-d, --download      Downloads the package
-#-u, --get-unpack    Downloads the package and unpacks it
-#-r, --root          Allows working as root (--asroot)";
-}
+sub generate {
+   my $pkg=shift;
+   info("Building package ".$pkg."...");
+   my $tar=$pkg.".tar.gz";
+   my $url="http://aur.archlinux.org/packages/".$pkg."/".$tar;
 
-sub kdownload {
-    my $pkg=shift;
-    my $tar=$pkg.".tar.gz";
-    my $url="http://aur.archlinux.org/packages/".$pkg."/".$tar;
-    info("Downloading the tarball...");
-    getstore($url, "./".$tar);
-}
+#let's download the file.
+   info("Downloading the tarball...");
+   getstore($url, "./".$tar);
 
-sub kunpack {
-    my $pkg=shift;
-    my $tar=$pkg.".tar.gz";
-    info("Extracting the tarball...");
-    my $archive = Archive::Any->new("./".$tar);
-    $archive->extract;
-}
+#untar it...
+   info("Extracting the tarball...");
+   my $archive = Archive::Any->new("./".$tar);
+   $archive->extract;
 
-sub kpackage {
-    my $pkg=shift;
-    info("Building package ".$pkg."...");
-
-    #let's download the file.
-    kdownload($pkg);
-
-    #untar it.
-    kunpack($pkg);
-
-    #build it.
-    system('cd '.$pkg.'; time makepkg -si');
-    info("Package ".$pkg." was successfully bulit.");
+#build it.
+   system('cd '.$pkg.'; time makepkg -si');
+   info("The build finished.");
 }
 
 my $argc = $#ARGV + 1;
 my $i = 0;
 if ($argc eq '0') {
-    print "ERROR: No package specified.";
-    help();
+   print "ERROR: No package specified.
+      HINT:  ./build.pl pkg [pkg2]\n";
 }
-
 while ($i != $argc) {
-    my $arg = $ARGV[$i]; #There was no other way.
-    kpackage($arg);
-    $i = $i + 1;
+   my $arg = $ARGV[$i]; #There was no other way.
+      generate($arg);
+   $i = $i + 1;
 }
