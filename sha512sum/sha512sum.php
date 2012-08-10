6604c5b0b64eb783c735489e9729c3b6186a6ff4
Kwpolska <kwpolska@gmail.com>
2012-08-10 11:50:52 +0200

    sha512sum: re-licensed, fixes
    Signed-off-by: Kwpolska <kwpolska@gmail.com>

--- sha512sum/sha512sum.php
+++ sha512sum/sha512sum.php
@@ -1,8 +1,41 @@
 <?php
-//sha512sum
-//By Kwpolska. On PD.
-$saltbase = 'somerandomsalt16'; //up to 16 characters, longer ones will be trimmed
+# sha512sum
+# Copyright (C) 2011, Kwpolska.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions, and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions, and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the author of this software nor the names of
+#    contributors to this software may be used to endorse or promote
+#    products derived from this software without specific prior written
+#    consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+$saltbase = 'xsomerandomsalt16'; //up to 16 characters, longer ones will be trimmed
 $salt = '$6$rounds=5000$'.$saltbase.'$';
-if($_SERVER['REQUEST_METHOD'] == 'POST') echo crypt($_POST['passwd'], $salt).'<br>';
-echo '<form method="post" action="?"><input type="password" name="passwd"> <input type="submit" value="Generate"></form>';
+if($_SERVER['HTTPS'] != on) echo '<p><strong>WARNING:</strong> not using HTTPS, the password will be sent in plaintext!</p>;
+
+echo '<p><form method="post" action="?"><input type="password" name="passwd"> <input type="submit" value="Generate"></form></p>';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') echo '<p>'.crypt($_POST['passwd'], $salt).'</p>';
 ?>
