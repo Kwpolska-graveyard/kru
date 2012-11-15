713d2ed0716585f35ae07e40d1aa293631c3d8f1
Kwpolska <kwpolska@gmail.com>
2012-11-15 20:53:15 +0100

    tau
    Signed-off-by: Kwpolska <kwpolska@gmail.com>

---
+++ miniutils/tau.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding=utf-8
+"""Implement τ (tau).  http://tauday.com/tau-manifesto to learn more."""
+import math
+math.tau = 2 * math.pi
+
+
+def nobullshit():
+    """Enable no bullshit mode."""
+    del math.pi
